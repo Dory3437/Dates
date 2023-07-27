b0b6284831f53dc63329fbe57b7f3a7860c96c1e
Sofia <ephermiral@gmail.com>
2023-07-27 13:50:26 -0400

    Initial Commit

---
+++ Dates/ContentView.swift
@@ -0,0 +1,51 @@
+//
+//  ContentView.swift
+//  Dates
+//
+//  Created by scholar on 7/26/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State private var date = Date()
+    @State private var titleQuestion = "Pick a date?"
+    
+    var body: some View {
+        
+        VStack(spacing: 50.0) {
+            
+            Text(titleQuestion)
+                .font(.title)
+                .fontWeight(.heavy)
+                .foregroundColor(Color.orange)
+                .multilineTextAlignment(.center)
+                .padding(.bottom)
+            
+            DatePicker(selection: $date, label: { Text("Dates") })
+                .padding(.bottom)
+            
+            Button("Schedule") {
+                
+                titleQuestion = "Don't be late for your very important date \(date)!"
+                //let formatted = Date().formatted()
+                
+            }
+            
+            .buttonStyle(.borderedProminent)
+            .tint(.orange)
+            .padding(.top)
+            
+        }
+        .padding()
+        
+        
+    }//closing view
+} //closing struct
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Dates/DatesApp.swift
@@ -0,0 +1,17 @@
+//
+//  DatesApp.swift
+//  Dates
+//
+//  Created by scholar on 7/26/23.
+//
+
+import SwiftUI
+
+@main
+struct DatesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
